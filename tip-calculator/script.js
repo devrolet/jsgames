d179961b0ac8cef26ce2219d0e46dd576a80576d
Chet Hill <chetdhill@gmail.com>
2023-03-22 15:17:00 -0500

    Calculate Cost

--- tip-calculator/script.js
+++ tip-calculator/script.js
@@ -1,5 +1,10 @@
 const button = document.querySelector("button");
+const output = document.querySelector(".output");
 
 console.log(button);
 
-button.innerHTML = "What's my tip?";
+button.addEventListener("click", function() {
+  const cost = document.querySelector("input");
+  console.log(cost.value*0.2);
+  output.innerText = "output";
+});
